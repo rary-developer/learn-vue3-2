ea16d150f41521568c05e4920d5f626ebb9306f8
rary <xogml6915@gmail.com>
2022-07-28 22:18:30 +0900

    Vue Practice day6

---
+++ src/lectures/12/App_v-memo.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<div v-memo="[subscribers]">
+			<p>subcribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<div>
+			<p>subcribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+	</div>
+	<button @click="subscribers++">Subs++</button>
+	<button @click="views++">Views++</button>
+	<button @click="likes++">Likes++</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+	setup() {
+		const subscribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(20);
+		return {
+			subscribers,
+			views,
+			likes,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
