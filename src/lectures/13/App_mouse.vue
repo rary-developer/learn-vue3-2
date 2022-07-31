fb727d4c8fff7e9d91c114abd112afee1f4ae257
rary <xogml6915@gmail.com>
2022-07-31 21:43:29 +0900

    Vue Practice day7

---
+++ src/lectures/13/App_mouse.vue
@@ -0,0 +1,70 @@
+<template>
+	<div>
+		<div id="modifiers">
+			<div @click="clickDiv">
+				<!-- <div @click.capture="clickDiv"></div> -->
+				DIV 영역
+				<p @click="clickP">
+					<!-- <p @click.self="clickP"></p> -->
+					<!-- <p @click.capture="clickP"></p> -->
+					P 영역
+					<!-- <span @click.stop="clickSpan"> SPAN 영역 </span> -->
+					<span @click="clickSpan"> SPAN 영역 </span>
+					<a href="https://naver.com" @click.prevent.stop="clickA"> A 영역 </a>
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		const clickDiv = () => {
+			console.log("clickDiv");
+			//location.href = "https://naver.com";
+		};
+
+		const clickP = () => {
+			console.log("clickP");
+		};
+
+		const clickSpan = () => {
+			console.log("clickSpan");
+			//alert("좋아요!");
+		};
+
+		const clickA = () => {
+			alert("어떤기능~");
+		};
+
+		return {
+			clickDiv,
+			clickP,
+			clickSpan,
+			clickA,
+		};
+	},
+};
+</script>
+
+<style scoped>
+#modifiers div,
+#modifiers p,
+#modifiers span {
+	padding: 40px;
+}
+
+#modifiers div {
+	background-color: #ccc;
+}
+
+#modifiers p {
+	background-color: #999;
+}
+
+#modifiers span {
+	background-color: #666;
+	display: block;
+}
+</style>
---
+++ src/lectures/15/App_multiple_source_type.vue
@@ -0,0 +1,74 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref, watch } from "vue";
+
+export default {
+	setup() {
+		const x = ref(0);
+		const y = ref(0);
+
+		const obj = reactive({
+			count: 0,
+		});
+
+		// watch(
+		// 	() => x.value + y.value,
+		// 	(sum, oldValue) => {
+		// 		console.log("sum: ", sum);
+		// 		console.log("oldVaLUE: ", oldValue);
+		// 	},
+		// );
+
+		//배열로 받기
+		// watch([x, y], (newX, newY) => {
+		// 	console.log("newX: ", newX);
+		// 	console.log("newY: ", newX);
+		// });
+
+		//object
+		// watch(
+		// 	() => obj.count,
+		// 	(newObj, olvObj) => {
+		// 		console.log("newObj: ", newObj);
+		// 	},
+		// );
+
+		// watch(obj, (newObj, oldObj) => {
+		// 	console.log("newObj: ", newObj);
+		// 	console.log("oldObj: ", oldObj);
+		// });
+
+		const person = reactive({
+			name: "홍길동",
+			age: 30,
+			hobby: "운동",
+			obj: {
+				count: 0,
+			},
+		});
+
+		// watch(person, newValue => {
+		// 	console.log("newValue: ", newValue);
+		// });
+
+		watch(
+			() => person.obj,
+			newValue => {
+				console.log("newValue: ", newValue);
+			},
+		);
+
+		return {
+			x,
+			y,
+			obj,
+			person,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
